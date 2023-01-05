987f57575dae3f6f9e0e6d554d29ef7c5aab6ca1
Jérôme Legrand <[jerome.legrand.79@gmail.com](mailto:jerome.legrand.79@gmail.com)>
2023-01-05 10:03:25 +0100

    candidate profile route, controller, and view

---
+++ src/Controller/CandidateController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Candidate;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/candidate')]
+class CandidateController extends AbstractController
+{
+    #[Route('/{id}', name: 'app_candidate_show', methods: ['GET'])]
+    public function show(Candidate $candidate): Response
+    {
+        return $this->render('candidate/show.html.twig', [
+            'user' => $candidate,
+        ]);
+    }
+}
