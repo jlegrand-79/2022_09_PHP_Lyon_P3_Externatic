5271962034c61b8cbf4eed7f47a0374c800ceb68
CharleneDR <chdarugna.polytech@gmail.com>
2023-01-09 18:54:56 +0100

    change of WYSIWYG config : restriction of font size and text format and add of a css as default config

---
+++ public/bundles/fosckeditor/config.js
@@ -0,0 +1,13 @@
+/**
+ * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function (config) {
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+    config.contentsCss = '/build/app.css';
+    config.fontSize_sizes = '10/10px;11/11px;12/12px;14/14px;16/16px;18/18px;20/20px;22/22px';
+    config.format_tags = 'p;h3;h4;h5;h6';
+};
