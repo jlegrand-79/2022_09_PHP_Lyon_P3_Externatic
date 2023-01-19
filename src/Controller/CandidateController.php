2ae14c7b6563055f4b5986c4f847b8de677d0845
AmauBE <amaury.beurrier@gmail.com>
2023-01-19 11:48:47 +0100

    Admin Rights and access

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -93,10 +93,48 @@ class CandidateController extends AbstractController
 
     #[Route('/{id}', name: 'app_candidate_admin_show', methods: ['GET'])]
     #[IsGranted('ROLE_ADMIN')]
-    public function showAdmin(Candidate $candidate): Response
+    public function showCandidateAdmin(Candidate $candidate): Response
     {
         return $this->render('candidate/show.html.twig', [
             'candidate' => $candidate,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_candidate_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function edit(Request $request, Candidate $candidate, CandidateRepository $candidateRepository): Response
+    {
+        $form = $this->createForm(CandidateType::class, $candidate);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $candidateRepository->save($candidate, true);
+
+            return $this->redirectToRoute('app_candidate_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('candidate/edit.html.twig', [
+            'candidate' => $candidate,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/new', name: 'app_candidate_admin_new', methods: ['GET', 'POST'])]
+    public function newCandidateAdmin(Request $request, CandidateRepository $candidateRepository, int $id, UserRepository $userRepository): Response
+    {
+        $candidate = new Candidate();
+        $candidate->setUser($userRepository->findOneById($id));
+        $form = $this->createForm(CandidateType::class, $candidate);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $candidateRepository->save($candidate, true);
+
+            return $this->redirectToRoute('app_candidate_index', [], Response::HTTP_SEE_OTHER);
+        }
+        return $this->renderForm('candidate/new.html.twig', [
+            'candidate' => $candidate,
+            'form' => $form,
         ]);
     }
 }
